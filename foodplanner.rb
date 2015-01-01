ea1b65650c849384948d1f8e1d27195199d99c69
Johan Walles <johan.walles@gmail.com>
2015-01-01 21:31:47 +0100

    Parse occations and names from the calendar

--- foodplanner.rb
+++ foodplanner.rb
@@ -13,11 +13,21 @@ food_yaml = ARGV[0]
 calendar_yaml = ARGV[1]
 
 # FIXME: Parse the food YAML file
+# FIXME: Verify it against a schema
 food_thing = YAML.load_file(food_yaml)
 
 # FIXME: Parse the calendar YAML file
+# FIXME: Verify it against a schema
 calendar_thing = YAML.load_file(calendar_yaml)
 
 # FIXME: Make a menu
+calendar_thing.each do |occation|
+  occation_name = occation.keys[0]
+  occation_participants = occation[occation_name]
+
+  # FIXME: ... make a list of all possible foods given the participants
+  # FIXME: ... and pick a course from that list
+  # FIXME: ... then don't forget to remove the course from the complete list
+end
 
 # FIXME: Print menu to stdout
