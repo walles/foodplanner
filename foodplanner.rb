6c5dbaed0da7343c37166205178fde8967f06c43
Johan Walles <johan.walles@gmail.com>
2015-01-01 11:35:06 +0100

    Remove non-mvp error handling

--- foodplanner.rb
+++ foodplanner.rb
@@ -9,18 +9,8 @@ if ARGV.length != 2
   $stderr.puts 'Syntax: foodplanner.rb <food YAML> <calendar YAML>'
   exit 1
 end
-
 food_yaml = ARGV[0]
-unless File.file?(food_yaml)
-  $stderr.puts "ERROR: not a file: #{food_yaml}"
-  exit 1
-end
-
 calendar_yaml = ARGV[1]
-unless File.file?(calendar_yaml)
-  $stderr.puts "ERROR: not a file: #{calendar_yaml}"
-  exit 1
-end
 
 # FIXME: Parse the food YAML file
 food_thing = YAML.load_file(food_yaml)
