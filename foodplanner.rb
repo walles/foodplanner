21ad7f960acef750901e9175aced80ba783205d0
Johan Walles <johan.walles@gmail.com>
2015-01-01 11:26:23 +0100

    Add simplistic command line parsing

--- foodplanner.rb
+++ foodplanner.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/env ruby
 
-# FIXME: Parse arguments
+# Parse arguments
+if ARGV.length != 2
+  $stderr.puts 'ERROR: Expected two arguments'
+  $stderr.puts
+  $stderr.puts 'Syntax: foodplanner.rb <food YAML> <calendar YAML>'
+  exit 1
+end
+
+food_yaml = ARGV[0]
+unless File.file?(food_yaml)
+  $stderr.puts "ERROR: not a file: #{food_yaml}"
+  exit 1
+end
+
+calendar_yaml = ARGV[1]
+unless File.file?(calendar_yaml)
+  $stderr.puts "ERROR: not a file: #{calendar_yaml}"
+  exit 1
+end
 
 # FIXME: Parse the food YAML file
 
