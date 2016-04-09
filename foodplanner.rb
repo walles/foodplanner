3bdec01da1e92db6b3e77d81f29ac6ca2ff37100
Johan Walles <johan.walles@gmail.com>
2016-04-09 16:08:30 +0200

    Count tags and print tag counts before the plan

--- foodplanner.rb
+++ foodplanner.rb
@@ -24,8 +24,11 @@ def available_food_for_occasion(food_thing, occasion)
     unless occasions.empty?
       next unless occasions.include?(occasion_name)
     end
+
     next if not_cooking.include?(cook)
-    next if (participants & not_eating).size > 0
+
+    non_eating_participants_count = (participants & not_eating).size
+    next if non_eating_participants_count > 0
 
     available_food << course
   end
@@ -68,22 +71,19 @@ def find_occasion_to_plan_for(food_thing, occasions)
   return candidates.sample
 end
 
-def plan_food(food_thing, calendar_thing)
-  plan = {}
-  remaining_occasions = calendar_thing.clone
+def update_tag_counts(tag_counts, course)
+  return unless course
 
-  until remaining_occasions.empty?
-    # Find the occasion that with the lowest number of available courses
-    occasion = find_occasion_to_plan_for(food_thing, remaining_occasions)
-    occasion_name = occasion.keys[0]
+  tags = course['tags']
+  return unless tags
 
-    course = plan_food_for_occasion(food_thing, occasion)
-    plan[occasion_name] = course
-    food_thing.delete(course)
-    remaining_occasions.delete(occasion)
+  tags.each do |tag|
+    tag_counts[tag] += 1
   end
+end
 
-  # Sort the plan to match the order of the calendar_thing
+# Sort the plan to match the order of the calendar_thing
+def order_plan(plan, calendar_thing)
   ordered_plan = []
   calendar_thing.each do |occasion|
     occasion_name = occasion.keys[0]
@@ -91,6 +91,30 @@ def plan_food(food_thing, calendar_thing)
   end
 
   return ordered_plan
+end
+
+def plan_food(food_thing, calendar_thing)
+  plan = {}
+  tag_counts = Hash.new(0)
+  remaining_occasions = calendar_thing.clone
+
+  until remaining_occasions.empty?
+    # Find the occasion that has the lowest number of available courses
+    occasion = find_occasion_to_plan_for(food_thing, remaining_occasions)
+    occasion_name = occasion.keys[0]
+
+    course = plan_food_for_occasion(food_thing, occasion)
+    plan[occasion_name] = course
+    update_tag_counts(tag_counts, food_thing[course])
+    food_thing.delete(course)
+    remaining_occasions.delete(occasion)
+  end
+
+  tag_counts.each_pair do |tag, value|
+    puts "#{tag}: #{value}"
+  end
+
+  return order_plan(plan, calendar_thing)
 end
 
 # FIXME: Optionally accept a third argument which is the output from a
